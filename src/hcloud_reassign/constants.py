bc8de311bc51d09436987eb3fb91e6d1ca5853fe
Christian Siegel <80589111+mrmolybdaen@users.noreply.github.com>
2025-01-01 04:49:48 +0100

    feat(constants.py): add constants for overall usage in the module

---
+++ src/hcloud_reassign/constants.py
@@ -0,0 +1,31 @@
+# Copyright: (c) 2024, Christian Siegel <molybdaen@mr42.org>
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""
+    This module contains constants used throughout hcloud_reassign
+"""
+import sys
+from sys import version, version_info, platform, prefix
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class EnvironmentalInfo:
+    # Note some system information
+    PYTHON_INFO = version
+    PYTHON_VERSION = version_info
+    PYTHON_EXECUTABLE = sys.executable
+    PYTHON_PLATFORM = platform
+
+    # Note some module information
+    SCRIPT_VERSION = "0.1.0"
+    SCRIPT_PATH = prefix
+
+# Configuration default section
+CONFIG_SECTION_CLIENT = 'client'
+
+# API endpoint URL
+CONFIG_DEFAULT_API_URL = 'https://api.hetzner.cloud/v1'
+
+# Configuration options
+CONFIG_OPTION_API_URL = 'api_url'
+CONFIG_OPTION_API_TOKEN = 'api_token'
